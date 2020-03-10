2de285eb0815b60d5d4fe4cc11d40bbd01e2fad9
yznik <yznik93@gmail.com>
2020-03-10 19:01:28 +0000

    with posix ipc

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 msg_receiver: main.cpp
-	g++ -std=c++11 -g -o msg_receiver main.cpp
+	g++ -std=c++11 -g -o msg_receiver main.cpp -lrt
--- main.cpp
+++ main.cpp
@@ -1,41 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <array>
 
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <fcntl.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <mqueue.h>
 #include <string.h>
 
-template<size_t SIZE>
-struct message {
-    long m_type;
-    char m_data[SIZE] = {0};
-    inline size_t capacity() const noexcept
-    {
-        return SIZE;
-    }
-    inline const char* data() const noexcept
-    {
-        return m_data;
-    }
-};
-
-inline bool IsExist(const std::string& name) noexcept
-{
-    struct stat buffer;
-    auto res = stat (name.c_str(), &buffer);
-    if (res == -1)
-    {
-        std::cerr << '[' << __FILE__ << ':' << __LINE__ << "] " << strerror(errno) << std::endl;
-        return true;
-    }
-    return res == 0; 
-}
-
-constexpr const char* const IN_PATH  = "/tmp/msg.temp";
-constexpr int PROJ_ID                = 1;
+constexpr const char* const IN_PATH  = "/test.mq";
 constexpr const char* const OUT_PATH = "/home/box/message.txt";
 
 #define CHECK_ERROR(CODE, file, line)                                                    \
@@ -49,23 +21,18 @@ if (CODE == -1)                                                                 
 
 int main()
 {
-    if (!IsExist(IN_PATH))
-    {
-        std::ofstream o(IN_PATH);
-    }
-    const auto key = ftok(IN_PATH, PROJ_ID);
-    CHECK(key)
-    int queueFlags = IPC_CREAT | 0666;
-    auto queueId = msgget(key, queueFlags);
+    mode_t perms = 0666;
+    int flags = O_RDWR | O_CREAT;
+    auto queueId = mq_open(IN_PATH, O_RDWR | O_CREAT, perms, nullptr);
     CHECK(queueId)
     std::ofstream f(OUT_PATH);
     while (true)
     {
-        message<80> msg;
-        auto received = msgrcv(queueId, &msg, msg.capacity(), 0, 0);
+        std::array<char, 8192> msg;
+        auto received = mq_receive(queueId, msg.data(), msg.size(), nullptr);
         CHECK(received)
         f << msg.data() << std::endl;
     }
-    CHECK(msgctl(queueId, IPC_RMID, nullptr))
+    CHECK(mq_close(queueId))
     return 0;
 }
